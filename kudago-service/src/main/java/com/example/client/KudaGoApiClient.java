ab54741f3491459b7e1ea4683174c6a0971511b7
GhostOfEndless <phyrocycle.moscow@yandex.ru>
2024-09-24 16:17:12 +0300

    Retrying enabled

--- kudago-service/src/main/java/com/example/client/KudaGoApiClient.java
+++ kudago-service/src/main/java/com/example/client/KudaGoApiClient.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -15,24 +16,30 @@ import java.util.List;
 
 @Slf4j
 @Component
+@EnableRetry
 @RequiredArgsConstructor
 public class KudaGoApiClient {
 
     private final RestClient restClient;
 
+    @Retryable(
+            maxAttemptsExpression = "${kudago.max-retries}",
+            backoff = @Backoff(delayExpression = "${kudago.retry-delay}"),
+            retryFor = {RuntimeException.class}
+    )
     public List<CategoryPayload> fetchCategories() {
         return fetchData("https://kudago.com/public-api/v1.4/place-categories/", CategoryPayload[].class);
-    }
-
-    public List<LocationPayload> fetchLocations() {
-        return fetchData("https://kudago.com/public-api/v1.4/locations/", LocationPayload[].class);
     }
 
     @Retryable(
             maxAttemptsExpression = "${kudago.max-retries}",
             backoff = @Backoff(delayExpression = "${kudago.retry-delay}"),
-            retryFor = RuntimeException.class
+            retryFor = {RuntimeException.class}
     )
+    public List<LocationPayload> fetchLocations() {
+        return fetchData("https://kudago.com/public-api/v1.4/locations/", LocationPayload[].class);
+    }
+
     private <T> List<T> fetchData(String uri, Class<T[]> responseType) {
         ResponseEntity<T[]> response = restClient.get()
                 .uri(uri)
@@ -43,10 +50,10 @@ public class KudaGoApiClient {
 
         if (body == null) {
             log.error("Received null response for URI: {}", uri);
-            throw new RuntimeException("Null response received");
+            throw new RuntimeException();
         } else if (!response.getStatusCode().is2xxSuccessful()) {
             log.error("Unexpected HTTP status code '{}' for URI: {}", response.getStatusCode(), uri);
-            throw new RuntimeException("Unexpected HTTP status code");
+            throw new RuntimeException();
         }
 
         log.debug("Successfully fetched '{}' entries from '{}'", body.length, uri);
