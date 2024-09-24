9e0cc1e9e6859a3aa3ea3d1a7bd18324f2623533
Aleksey Kabanov <phyrocycle.moscow@yandex.ru>
2024-09-24 15:20:25 +0300

    RestClient and Spring Retry added

--- kudago-service/src/main/java/com/example/client/KudaGoApiClient.java
+++ kudago-service/src/main/java/com/example/client/KudaGoApiClient.java
@@ -1,59 +1,55 @@
 package com.example.client;
 
-import com.example.config.KudaGoClientProperties;
 import com.example.controller.payload.CategoryPayload;
 import com.example.controller.payload.LocationPayload;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Supplier;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class KudaGoApiClient {
 
-    private final KudaGoClientProperties properties;
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
 
     public List<CategoryPayload> fetchCategories() {
-        return fetchDataWithRetry("https://kudago.com/public-api/v1.4/place-categories", CategoryPayload[].class);
+        return fetchData("https://kudago.com/public-api/v1.4/place-categories/", CategoryPayload[].class);
     }
 
     public List<LocationPayload> fetchLocations() {
-        return fetchDataWithRetry("https://kudago.com/public-api/v1.4/locations", LocationPayload[].class);
+        return fetchData("https://kudago.com/public-api/v1.4/locations/", LocationPayload[].class);
     }
 
-    private <T> List<T> fetchDataWithRetry(String url, Class<T[]> responseType) {
-        return CompletableFuture.supplyAsync((Supplier<List<T>>) () -> {
-            for (int attempt = 0; attempt < properties.getMaxRetries(); attempt++) {
-                try {
-                    T[] response = restTemplate.getForObject(url, responseType);
-                    if (response == null) {
-                        return Collections.emptyList();
-                    }
-                    log.debug("Successfully fetched '{}' entries!", response.length);
-                    return Arrays.asList(response);
-                } catch (Exception e) {
-                    log.error(e.getMessage());
-                    try {
-                        //noinspection BusyWait
-                        Thread.sleep(properties.getRetryDelay());
-                    } catch (InterruptedException ie) {
-                        Thread.currentThread().interrupt();
-                        throw new RuntimeException("Thread interrupted during retry wait", ie);
-                    }
-                }
-            }
+    @Retryable(
+            maxAttemptsExpression = "${kudago.max-retries}",
+            backoff = @Backoff(delayExpression = "${kudago.retry-delay}"),
+            retryFor = RuntimeException.class
+    )
+    private <T> List<T> fetchData(String uri, Class<T[]> responseType) {
+        ResponseEntity<T[]> response = restClient.get()
+                .uri(uri)
+                .retrieve()
+                .toEntity(responseType);
 
-            log.error("Failed to fetch data after {} attempts", properties.getMaxRetries());
-            return Collections.emptyList();
-        }).join();
+        var body = response.getBody();
+
+        if (body == null) {
+            log.error("Received null response for URI: {}", uri);
+            throw new RuntimeException("Null response received");
+        } else if (!response.getStatusCode().is2xxSuccessful()) {
+            log.error("Unexpected HTTP status code '{}' for URI: {}", response.getStatusCode(), uri);
+            throw new RuntimeException("Unexpected HTTP status code");
+        }
+
+        log.debug("Successfully fetched '{}' entries from '{}'", body.length, uri);
+        return Arrays.asList(body);
     }
 }
